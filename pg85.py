c478556ee71ad99b36264af8d4139b8804bb39d8
h-c-n <95289853+h-c-n@users.noreply.github.com>
2022-02-05 09:14:17 +0530

    Add files via upload

--- pg85.py
+++ pg85.py
@@ -1,14 +1,17 @@
-filen=open("tttt.txt",'r')
-fn1=open("ttt.txt",'w')
-cont=filen.readlines()
-for i in range(0,len(cont)):
- if(i % 2 == 0):
-  fn1.write(cont[i])
- else:
-  pass
-fn1.close()
-fn1=open("ttt.txt",'r')
-cont1=fn1.readlines()
-print(cont1)
-filen.close()
-fn1.close()
+import csv
+
+with open('temp.csv', mode='w') as outf:
+
+ fieldnames = ['Name', 'Department', 'Birthday Month']
+
+ content = csv.DictWriter(outf, fieldnames=fieldnames)
+
+ content.writeheader()
+
+ content.writerow({'Name': 'John', 'Department': 'Accounting', 'Birthday Month': 'November'})
+
+ content.writerow({'Name': 'Amy', 'Department': 'IT', 'Birthday Month': 'March'})
+data = csv.DictReader(open("temp.csv"))
+print("CSV file as a dictionary:\n")
+for row in data:
+   print(row)
